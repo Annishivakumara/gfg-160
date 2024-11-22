de146542ce737c011e90df1ebd5d057b0d2845a3
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-11-22 23:25:34 +0530

    Update 02.java

--- 02.java
+++ 02.java
@@ -1,28 +1,27 @@
-//                                                 #Move All Zeroes to End
+/*
+Move All Zeroes to End
+Given an array arr[]. Push all the zeros of the given array to the right end of the array while maintaining the order of non-zero elements.
+Do the mentioned change in the array in place.
+
+Examples:
+
+Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+Output: [1, 2, 4, 3, 5, 0, 0, 0]
+Explanation: There are three 0s that are moved to the end.
+ */
 class Solution {
-    // Function to push all zeros to the end
     void pushZerosToEnd(int[] arr) {
         int n = arr.length;
-
-        // Create a temporary array to store the non-zero elements
         int[] temp = new int[n];
-
-        // Pointer to keep track of index in temp[]
         int j = 0;
-
-        // Copy non-zero elements to temp[]
         for (int i = 0; i < n; i++) {
             if (arr[i] != 0) {
                 temp[j++] = arr[i];
             }
         }
-
-        // Fill the remaining positions in temp[] with zeros
         while (j < n) {
             temp[j++] = 0;
         }
-
-        // Copy the elements back from temp[] to arr[]
         for (int i = 0; i < n; i++) {
             arr[i] = temp[i];
         }
