1e72dfeb078e0e49a4b1963bc312e0ca383eec32
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-11-26 22:55:16 +0530

    Update day-09.java

--- day-09.java
+++ day-09.java
@@ -1,3 +1,14 @@
+/*
+Maximum Product Subarray
+Given an array arr[] that contains positive and negative integers (may contain 0 as well). Find the maximum product that we can get in a subarray of arr.
+
+Note: It is guaranteed that the output fits in a 32-bit integer.
+
+Examples
+
+Input: arr[] = [-2, 6, -3, -10, 0, 2]
+Output: 180
+Explanation: The subarray with maximum product is {6, -3, -10} with product = 6 * (-3) * (-10) = 180.*/
 class Solution {
     int getMinDiff(int[] arr, int k) {
         int n=arr.length;
